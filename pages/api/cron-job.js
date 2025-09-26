6b5e4735b48f9815d5c6b9d62e6dcaf956311300
Juan90264 <68405238+Juan90264@users.noreply.github.com>
2025-09-26 12:29:42 -0300

    Create cron-job.js

---
+++ pages/api/cron-job.js
@@ -0,0 +1,15 @@
+// api/cron-job.js
+
+export default async function handlr(req, res) {
+  console.log("Cron job acionado!");
+
+  try {
+    const response = await fetch();
+    const data = await response.json();
+    console.log("Resposta do site externo:", data);
+    res.status(200).json({ message: "Tarefa de cron executado com sucesso!" });
+  } catch (error) {
+    console.error("Erro na tarefa de cron:", error);
+    res.status(500).json({ message: "Erro ao executar a tarefa de cron." });
+  }
+}
